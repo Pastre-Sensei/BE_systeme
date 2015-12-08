45b21f2349ee8d45566750e2476ec6885da0d0d1
koubriah <koubriah@gmail.com>
2015-12-08 15:17:04 +0100

    [Projet_systeme.h] version 1.2

--- BE_systeme/projet_systeme.h
+++ BE_systeme/projet_systeme.h
@@ -9,19 +9,18 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 /* gcc -Wall -pthread -o projet_systeme projet_systeme.c*/
-
-typedef struct {
+typedef struct
+{
     pthread_t id_abonne;
     int nbre_messages;
     int id_file_desc;
 } abonne;
-
-typedef struct {
+typedef struct
+{
     pthread_t destinataire;
     pthread_t expediteur;
     char * msg;
 } message;
-
 abonne *tab_abonnes;
 int nombre_max_abonnes;
 int nombre_abonne = 0;
@@ -31,9 +30,8 @@ int flag_gestionnaire = 0;
 int flag_rcvMsg = 0;
 pthread_t id_gestionnaire;
 pthread_cond_t _var_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t _var_cond_rcv = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t _mutex = PTHREAD_MUTEX_INITIALIZER;
-
-
 //Déclarations des fonctions
 int initMsg(int a, int b, int c);
 int aboMsg(pthread_t a);
@@ -43,6 +41,5 @@ int rcvMsg(pthread_t id, int nbre_msg);
 int finMsg(int flag);
 void * gestionnaire(void * arg);
 int test_gestionnaire(void);
-
 #endif // includes
 
